c42435e05d92b92f5e6b6728e8d2c93104d2b0f2
adamnurlign <adamnurl11@gmail.com>
2025-10-07 14:00:33 +0000

    What is your name?

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,6 +13,9 @@ export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("name")) {
     return "Rohan";
   }
+  if (query.toLowerCase().includes("What is your name?")) {
+    return "ain";
+  }
 
   return "";
 }
